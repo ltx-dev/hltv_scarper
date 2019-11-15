2a798c768b9bd90cf6638706069e4ef2a277db40
ltx-dev <chrislotix@gmail.com>
2019-11-15 14:38:50 +0100

    Add player name from the scraped website for better user experience

--- hltv_scraper.py
+++ hltv_scraper.py
@@ -11,7 +11,9 @@ import re
 player_id = argv[1]
 kill_threshold = float(argv[2])
 list_of_kills = []
-url = "https://www.hltv.org/stats/players/matches/{}/JUGi?startDate=2019-08-14&endDate=2019-11-14".format(player_id)
+# url = "https://www.hltv.org/stats/players/matches/{}/JUGi?startDate=2019-08-14&endDate=2019-11-14".format(player_id)
+url = "https://www.hltv.org/stats/players/matches/{}/x?startDate=2019-08-14&endDate=2019-11-14".format(player_id)
+
 k_regex = '^[0-9]{1,2}'
 
 
@@ -21,11 +23,22 @@ parser = BeautifulSoup(source.text, "html.parser")
 list_of_kills = parser.find_all('td', class_="statsCenterText")
 list_of_kills = [int(re.match(k_regex, element.text.strip()).group()) for element in list_of_kills]
 
+player_name = parser.find('span', class_="standard-headline")
+player_name = player_name.text.split(' ')[-1] # this probably can be done better
+
 below_threshold = [n for n in list_of_kills if n < kill_threshold]
 # get the percentage of matches below given threshold
 below_percentage = (len(below_threshold) / len(list_of_kills)) * 100
 above_percentage = 100 - below_percentage
 
+print(player_name)
 print("Below threshold: {0:.2f}%".format(below_percentage))
 print("Above threshold: {0:.2f}%".format(above_percentage))
-# print("{}/{}".format(len(below_threshold), len(list_of_kills)))
+#
+# XANT 18.5, 7938
+# tabseN 19.5 5794
+# smooya 17.5 11271
+# hunter 19.5 3972
+# kennys 18.5 7167
+# nexa 16.5 9618
+#
